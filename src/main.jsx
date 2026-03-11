8d6b0f30d0ca314e101241b5e540bcb57bfaf983
devhemanthac-commits <dev.hemanth.ac@gmail.com>
2026-03-11 20:45:43 +0530

    fix: add basename to BrowserRouter for proper routing

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { CartProvider } from './context/CartContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Aroha/">
       <CartProvider>
         <App />
       </CartProvider>
